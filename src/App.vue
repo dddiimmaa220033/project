d3ed0670ccbd8a1b474f1c8ca99b93b8fc7a58d2
Dovzhenko_IPZ-23006b <ipz23-d.dovzhenko@nubip.edu.ua>
2024-07-13 14:54:36 +0300

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import MyHello from './components/MyHello.vue'
+ import Header from './components/Header.vue';
 </script>
 
-<template></template>
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+    
+    <h1>Dmytro Dovzhenko</h1>
+  </div>
+</template>
 
-<style scoped></style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="flex justify-between border-b border-slate-300 px-8 py-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10" />
+        <div>
+          <h2 class="text-lx font-bold uppercase">RunCity</h2>
+          <p class="text-neutral-500">Магазин найкращих кросівок</p>
+        </div>
+      </div>
+  
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3 text-neutral-500 hover:text-black cursor-pointer">
+          <img src="/cart.svg" alt="Cart" />
+          <b>1205 грн.</b>
+        </li>
+  
+        <li class="flex items-center gap-3 text-neutral-500 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="Cart" />
+          <span>Улюблене</span>
+        </li>
+  
+        <li class="flex items-center gap-3 text-neutral-500 hover:text-black cursor-pointer">
+          <img src="/profile.svg" alt="Cart" />
+          <span>Профіль</span>
+        </li>
+      </ul>
+    </header>
+  </template>
+  
