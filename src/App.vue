e31a5817e757fcc814d2838ae35a7b550ea43288
Dovzhenko_IPZ-23006b <ipz23-d.dovzhenko@nubip.edu.ua>
2024-07-14 11:44:52 +0300

    Card

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <script setup>
  import Header from './components/Header.vue';
+ import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
-    
    <div>
-    <div>
-      <img src="/like-1.svg" alt="Like 1" />
-      <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-      <p>Nike Blazer Mid Suede</p>
-
-      <div class="flex justify-between">
-        <div>
-          <span>Ціна:</span>
-          <span>1205 грн.</span>
-        </div>
-
-        <img src="/plus.svg" alt="Plus" />
-
-      </div>
-    </div>
-   
-  
+    <Card />
   </div>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
+      <img src="/like-2.svg" alt="Like 1" class="absolute top-8 left-8" />
+      <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+      <p class="mt-2">Nike Blazer Mid Suede</p>
+
+      <div class="flex justify-between mt-5">
+        <div class="flex flex-col">
+          <span class="text-slate-400">Ціна:</span>
+          <b>1205 грн.</b>
+        </div>
+
+        <img src="/plus.svg" alt="Plus" />
+
+      </div>
+    </div>
+</template>
