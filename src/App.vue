5a8b548f3b369908ee37ef1aff47ce0d03a71784
Dmytro Dovzhenko <ipz23-d.dovzhenko@nubip.edu.ua>
2024-07-17 13:42:47 +0300

    Search+filter

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,23 @@ import Drawer from './components/Drawer.vue'
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Всі Кросівки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Всі Кросівки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+          <option>За назвою</option>
+          <option>За ціною (недорогі)</option>
+          <option>За ціною (дорогі)</option>
+        </select>
+        <div class="relative"> 
+          <img class="absolute left-4 top-3" src="/search.svg"/>
+          <input class="border rounded-md py-2 pl-11 pr-4 outline-none"
+          type="text" 
+          placeholder="Пошук..." />
+        </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
